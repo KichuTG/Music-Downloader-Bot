62939885b0f0e3396ce1caa641ebbc2b87cb46ee
KichuTG <kichumon333@outlook.com>
2024-11-02 10:53:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ class Config(object):
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001678094109))
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     SESSION_NAME = os.environ.get("SESSION_NAME", "spotdown")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1905251964"))
